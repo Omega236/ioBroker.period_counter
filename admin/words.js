80a038dfbadc0cb32f2bc5a22acb44e1a78d6e40
Omega <general.of.omega@googlemail.com>
2020-09-10 19:01:43 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"period_counter adapter settings": {
+		"en": "Adapter settings for period_counter",
+		"de": "Adaptereinstellungen für period_counter",
+		"ru": "Настройки адаптера для period_counter",
+		"pt": "Configurações do adaptador para period_counter",
+		"nl": "Adapterinstellingen voor period_counter",
+		"fr": "Paramètres d'adaptateur pour period_counter",
+		"it": "Impostazioni dell'adattatore per period_counter",
+		"es": "Ajustes del adaptador para period_counter",
+		"pl": "Ustawienia adaptera dla period_counter",
+		"zh-cn": "period_counter的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
